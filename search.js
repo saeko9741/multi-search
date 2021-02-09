7f95d7aecaafcb2cfefabdcac0e013d3981f9ba7
saeko9741 <saeko9741@gmail.com>
2021-02-09 18:56:06 +0900

    Fix link

--- search.js
+++ search.js
@@ -1,7 +1,8 @@
 function onButtonClick() {
   var searched_text = document.getElementById("search_text").value;
-  
-  window.open(`https://www.google.com/search?q=${searched_text} 意味`);
+
+  window.open(`https://ejje.weblio.jp/content/${searched_text}`);
   window.open(`https://www.google.com/search?q=${searched_text} 語源`);
   window.open(`https://www.google.com/search?q=${searched_text}`); 
+  window.open(`https://dictionary.cambridge.org/ja/dictionary/english/${searched_text}`);  
 };
