a2510bd45c45c5c1d6710ad98d45dfe2376d13df
saeko9741 <saeko9741@gmail.com>
2021-02-14 22:58:00 +0900

    Move files

---
+++ public/search.js
@@ -0,0 +1,13 @@
+function onButtonClick() {
+  var searched_text = document.getElementById("search_text").value;
+
+  if (searched_text === '' || searched_text.length < 2){
+    return false;
+  }else {
+    window.open(`https://ejje.weblio.jp/content/${searched_text}`);
+    window.open(`https://www.google.com/search?q=${searched_text} 語源`);
+    window.open(`https://www.google.com/search?q=${searched_text}`); 
+    window.open(`https://dictionary.cambridge.org/ja/dictionary/english/${searched_text}`);  
+  };
+  
+};
