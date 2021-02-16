6ff833b47624978075af0939f8ba01bbf079d696
saeko9741 <saeko9741@gmail.com>
2021-02-16 10:17:26 +0900

    Fix image link

--- public/search.js
+++ public/search.js
@@ -1,4 +1,4 @@
-function onButtonClick() {
+function ButtonClick() {
   var searched_text = document.getElementById("search_text").value;
 
   if (searched_text === '' || searched_text.length < 2){
@@ -6,8 +6,7 @@ function onButtonClick() {
   }else {
     window.open(`https://ejje.weblio.jp/content/${searched_text}`);
     window.open(`https://www.google.com/search?q=${searched_text} 語源`);
-    window.open(`https://www.google.com/search?q=${searched_text}`); 
+    window.open(`https://www.google.com/search?q=${searched_text}&tbm=isch`); 
     window.open(`https://dictionary.cambridge.org/ja/dictionary/english/${searched_text}`);  
   };
-  
 };
